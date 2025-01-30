621f84a803a1bdcf7a8b17880479afcffbe5b426
Lyedson Matheus dos Santos Silva <20221074010011@ifrn.local>
2025-01-30 14:05:24 -0300

    First Commit

---
+++ src/lyedsonmatheusdss/LyedsonMatheusDSS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package LyedsonMatheusDSS;
+import java.sql.*;
+/**
+ *
+ * @author 20221074010011
+ */
+public class LyedsonMatheusDSS{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // o codigo ta no main.java
+    }
+}
